96c435d61dd262dbbf2008329227c5c366eeaca1
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2021-01-30 15:38:38 +0300

    Lesson 42, Authorization Service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
 import { LoginPageComponent } from './login-page/login-page.component'
@@ -21,9 +22,10 @@ import { RegisterPageComponent } from './register-page/register-page.component'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  //providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
