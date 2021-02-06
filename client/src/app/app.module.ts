8f3c6463c4c16c5bd6c69b6da2da38c2aef581d6
Ilias Aidar <Ilias.Aidar@ismarty.pro>
2021-02-06 15:52:40 +0300

    Lesson 45. Token Interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
 import { LoginPageComponent } from './login-page/login-page.component'
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component'
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component'
 import { RegisterPageComponent } from './register-page/register-page.component'
+import { TokenInterceptor } from './shared/classes/token.interceptor'
+
 
 @NgModule({
   declarations: [
@@ -25,6 +27,14 @@ import { RegisterPageComponent } from './register-page/register-page.component'
     ReactiveFormsModule,
     HttpClientModule
   ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: TokenInterceptor,
+    }
+  ],
+
   //providers: [],
   bootstrap: [AppComponent]
 })
